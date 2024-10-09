b720edcd0aa9be376b8a043feebc0d244147dafa
koding88 <duongngocanh2k03@gmail.com>
2024-10-09 16:20:10 +0700

    fix deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import connectDB from './db/connectDB.js';
 import { app, server } from './socket/socket.js';
+import path from 'path';
 
 
 dotenv.config();
@@ -16,10 +17,18 @@ app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/messages/", messageRoutes);
 app.use("/api/users/", userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
